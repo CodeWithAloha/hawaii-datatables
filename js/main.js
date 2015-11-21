c6e438f8d97599f86dddce1dae917b465f43a7b3
Ryan <ryankanno@localkinegrinds.com>
2015-11-21 07:37:47 -1000

    Added in timestamp conversion func

--- js/main.js
+++ js/main.js
@@ -55,8 +55,12 @@ function writeTableWith(data, tabletop) {
         tabletop.data() :
         _.reduce(tabletop.sheets(), function(memo, num){ return memo.concat(num.all()); }, []);
 
-    // Autolink URLs
-    all_data = _.map(all_data, function(v) { return _.mapValues(v, function(v) { return v.autoLink(); }); });
+    // Autolink / timestamps
+    // TODO: To be generic, this should be passed in from the caller
+    all_data = _.map(all_data, function(v) {
+      return _.mapValues(v, function(v,k,o) {
+        return (k == "last_updated" && v) ? convertTimestamp(v) : v.autoLink();
+    })});
 
     jqueryNoConflict("#data-container").replaceWith(table);
 
@@ -77,3 +81,29 @@ jQuery.fn.dataTableExt.oSort["string-case-asc"]  = function(x,y) {
 jQuery.fn.dataTableExt.oSort["string-case-desc"] = function(x,y) {
 	return ((x < y) ?  1 : ((x > y) ? -1 : 0));
 };
+
+// https://gist.github.com/kmaida/6045266
+function convertTimestamp(timestamp) {
+  var d = new Date(timestamp * 1000),	// Convert the passed timestamp to milliseconds
+		yyyy = d.getFullYear(),
+		mm = ('0' + (d.getMonth() + 1)).slice(-2),	// Months are zero based. Add leading 0.
+		dd = ('0' + d.getDate()).slice(-2),			// Add leading 0.
+		hh = d.getHours(),
+		h = hh,
+		min = ('0' + d.getMinutes()).slice(-2),		// Add leading 0.
+		ampm = 'AM',
+		time;
+
+	if (hh > 12) {
+		h = hh - 12;
+		ampm = 'PM';
+	} else if (hh === 12) {
+		h = 12;
+		ampm = 'PM';
+	} else if (hh == 0) {
+		h = 12;
+	}
+
+	// ie: 2013-02-18, 8:35 AM
+	return yyyy + '-' + mm + '-' + dd + ', ' + h + ':' + min + ' ' + ampm;
+}
