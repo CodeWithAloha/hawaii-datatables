74e48faafc909811b403cd53e57d478c5d610675
Alex Cabello <alexander.cabello@gmail.com>
2016-03-11 18:22:03 -1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ function writeTableWith(data, tabletop) {
 
     table.DataTable({
         "iDisplayLength": 200,
-        "paging", false,
+        "paging": false,
         "aaData": all_data,
         "aoColumns": columns,
         "oLanguage": {
@@ -72,29 +72,3 @@ jQuery.fn.dataTableExt.oSort["string-case-asc"]  = function(x,y) {
 jQuery.fn.dataTableExt.oSort["string-case-desc"] = function(x,y) {
 	return ((x < y) ?  1 : ((x > y) ? -1 : 0));
 };
-
-// https://gist.github.com/kmaida/6045266
-function convertTimestamp(timestamp) {
-  var d = new Date(timestamp * 1000),	// Convert the passed timestamp to milliseconds
-		yyyy = d.getFullYear(),
-		mm = ('0' + (d.getMonth() + 1)).slice(-2),	// Months are zero based. Add leading 0.
-		dd = ('0' + d.getDate()).slice(-2),			// Add leading 0.
-		hh = d.getHours(),
-		h = hh,
-		min = ('0' + d.getMinutes()).slice(-2),		// Add leading 0.
-		ampm = 'AM',
-		time;
-
-	if (hh > 12) {
-		h = hh - 12;
-		ampm = 'PM';
-	} else if (hh === 12) {
-		h = 12;
-		ampm = 'PM';
-	} else if (hh == 0) {
-		h = 12;
-	}
-
-	// ie: 2013-02-18, 8:35 AM
-	return yyyy + '-' + mm + '-' + dd + ', ' + h + ':' + min + ' ' + ampm;
-}
